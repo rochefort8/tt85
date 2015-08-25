6027a09d988ed346ce30d5b67c58618b59b07e75
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2015-08-25 09:49:51 +0000

    Update.sh

--- registrator/update.sh
+++ registrator/update.sh
@@ -7,4 +7,14 @@ php get_email_data.php
 ./remove_dup.sh  tmp/non-participant-raw.csv tmp/non-participant_a.csv 
 ./extract_new.sh tmp/participant.csv tmp/participant_a.csv tmp/participant_delta.csv
 ./extract_new.sh tmp/non-participant.csv tmp/non-participant_a.csv tmp/non-participant_delta.csv
-php put_data.php tmp/participant_delta.csv tmp/non-participant_delta.csv 
+
+if [ -f tmp/non-participant_delta.csv -o -f tmp/participant_delta.csv ]; then
+    echo "New data."
+    mv tmp/participant_a.csv tmp/participant.csv 
+    mv tmp/non-participant_a.csv tmp/non-participant.csv 
+
+    php put_data.php tmp/participant_delta.csv tmp/non-participant_delta.csv 
+
+else
+    echo "No new data"
+fi
