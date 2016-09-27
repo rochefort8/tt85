470fe92b1616e07b297ca83806a2c9e26ef728f5
Ubuntu <ubuntu@ip-172-31-21-162.ap-northeast-1.compute.internal>
2016-09-27 07:43:31 +0000

    Add web page on docker for VivaBelgianBeer beer registration

---
+++ parse-web-client/vbb-beer/confirm.php
@@ -0,0 +1,71 @@
+<?php
+$text_param1 = isset($_POST["text_param1"])? $_POST["text_param1"] : "";
+$text_param2 = isset($_POST["text_param2"])? $_POST["text_param2"] : "";
+$text_param3 = isset($_POST["text_param3"])? $_POST["text_param3"] : "";
+$file_param1 = isset($_FILES["file_param1"]["name"])? $_FILES["file_param1"]["name"] : "" ; 
+
+if (is_uploaded_file($_FILES["file_param1"]["tmp_name"])) {
+  if (move_uploaded_file($_FILES["file_param1"]["tmp_name"], "/tmp/" . $_FILES["file_param1"]["name"])) {
+    chmod("/tmp/" . $_FILES["file_param1"]["name"], 0644);
+  } else {
+    echo "ファイルをアップロードできません。";
+  }
+} else {
+  echo "ファイルが選択されていません。";
+}
+$file_param1_uploaded_path = "/tmp/" . $_FILES["file_param1"]["name"] ;
+?>
+
+<head>
+
+<meta charset = "UTF-8">
+<title>ニュース</title>
+<link rel="stylesheet" href="./contact.css" type="text/css" />
+</head>
+
+<body>
+<div class="main">
+<div id="contactInfo">
+
+     <h1>ニュース</h1>
+
+     <form name="form1" action="./send.php" method="post">
+     <input type="hidden" name="text_param1"  value="<?php echo htmlspecialchars($text_param1, ENT_QUOTES, "UTF-8");?>">
+     <input type="hidden" name="text_param2"  value="<?php echo htmlspecialchars($text_param2, ENT_QUOTES, "UTF-8");?>">
+     <input type="hidden" name="text_param3"  value="<?php echo htmlspecialchars($text_param3, ENT_QUOTES, "UTF-8");?>">
+     <input type="hidden" name="file_param1" value="<?php echo htmlspecialchars($file_param1, ENT_QUOTES, "UTF-8");?>">
+     <input type="hidden" name="file_param1_uploaded_path" 
+     	    value="<?php echo htmlspecialchars($file_param1_uploaded_path, ENT_QUOTES, "UTF-8");?>">
+     <table  class="contact_tbl">
+
+     <tr>
+       <th>Name</th>
+         <td><?php echo htmlspecialchars($text_param1, ENT_QUOTES, "UTF-8");?></td>
+     </tr>
+
+     <tr>
+       <th>名称</th>
+         <td><?php echo htmlspecialchars($text_param2, ENT_QUOTES, "UTF-8");?></td>
+     </tr>
+
+     <tr>
+       <th>説明</th>
+         <td><?php echo htmlspecialchars($text_param3, ENT_QUOTES, "UTF-8");?></td>
+     </tr>
+     <tr>
+       <th>画像</th>
+       <td><?php echo nl2br(htmlspecialchars($file_param1, ENT_QUOTES, "UTF-8"));?></td>
+     </tr>
+     </table>
+
+    <div id="btn_area">
+      <input type="button" name="back_btn" value="戻る"
+         onclick="form1.action='./index.php';form1.submit();" class="btn"> &nbsp;&nbsp;
+      <input type="submit" name="next_btn" value="送信" class="btn">
+    </div>
+</form>
+
+</div><!--/contactInfo-->
+</div><!--/main-->
+</body>
+</html>
