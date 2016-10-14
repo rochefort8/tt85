4e4034160b79d933bd09927d92afed89b2515796
Ubuntu <ubuntu@ip-172-31-21-162.ap-northeast-1.compute.internal>
2016-10-14 10:26:37 +0000

    Add sql script

--- server_root/docker/cmd/sql_insert.sh
+++ server_root/docker/cmd/sql_insert.sh
@@ -2,31 +2,30 @@
 
 set -x
 
-customer_name="Yuji"
-
 
 folder_id=
-customer_type=
-customer_name=""
-customer_ruby=""
+customer_type
+customer_id="123"
+customer_name="Yuji"
+customer_ruby="AAA"
 customer_juniorhighschool=""
 customer_club=""
 customer_couple=""
 customer_role=""
-customer_postcode=""
+customer_postcode="2760022"
 customer_address=""
 customer_addressruby=""
 customer_phone=""
 customer_graduate=""
 customer_mobile=""
-customer_email=""
-customer_id=""
+customer_email="ogihara@gmail.com"
 customer_gender=""
 customer_comment=""
 customer_role=""
 customer_annualfee=""
 
+docker exec -it tt-server mysql -uroot db -e "INSERT INTO customer_customer ( folder_id,customer_type,customer_id,customer_name,customer_ruby,customer_gender,customer_graduate,customer_email,customer_phone,customer_mobile,customer_postcode,customer_address,customer_addressruby,customer_juniorhighschool,customer_club,customer_couple,customer_role,customer_annualfee,customer_party,customer_comment) VALUES ('$folder_id','$customer_type','$customer_id','$customer_name','$customer_ruby','$customer_gender','$customer_graduate','$customer_email','$customer_phone','$customer_mobile','$customer_postcode','$customer_address','$customer_addressruby','$customer_juniorhighschool','$customer_club','$customer_couple','$customer_role','$customer_annualfee','$customer_party','$customer_comment')" 
 
-docker exec -it tt-server mysql -uroot db -e "INSERT INTO customer_customer ( folder_id,customer_type,customer_id,customer_name,customer_ruby,customer_gender,customer_graduate,customer_email,customer_phone,customer_mobile,customer_postcode,customer_address,customer_addressruby,customer_juniorhighschool,customer_club,customer_couple,customer_role,customer_annualfee,customer_party,customer_comment) VALUES ('','','','$customer_name','','','','','','','','','','','','','','','','')"
+
 
 
